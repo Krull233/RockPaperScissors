d1ebf86ef7e5acc31f6e1109514c17291546d772
Megan Holland-Robinson <1995megan@gmail.com>
2022-03-29 12:03:16 -0400

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
-function computerPlay() {
-  const choices = ["Rock", "Paper", "Scissors"];
-  const computerSelection = choices[Math.floor(Math.random() * choices.length)];
-  return computerSelection;
+const choices = ["rock", "paper", "scissors"];
+
+function computerPlay(choices) {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// have the player pick rock, paper, or scissors
-// have the computer pick rock, paper, or scissors
-// rock > scissors | paper > rock | scissors > paper
-// declare a winner
-// make it case-insensitive
+function playRound() {
+  computerSelection = computerPlay(choices);
+  `Computer has chosen ${computerSelection}`;
+  playerSelection = prompt().toLowerCase();
+  `Player has chosen ${playerSelection}`;
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    result = "Player has won this round!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    result = "Player has won this round!";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    result = "Player has won this round!";
+  } else if (playerSelection == computerSelection) {
+    result = "It's a tie!";
+  } else {
+    result = "Computer has won this round!";
+  }
+  return result;
+}
